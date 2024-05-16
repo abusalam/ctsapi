d3f3a77ec3440e1db6bf800779d400ba8c0af921
itsmanwar <102802707+itsmanwar@users.noreply.github.com>
2024-05-16 16:01:30 +0530

    sc
    This reverts commit d17164cd78f657c3fe35691ef74fa6a6aa241a20.

---
+++ DAL/Entities/ChequeDistribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using NpgsqlTypes;
+
+namespace CTS_BE.DAL.Entities;
+
+[Keyless]
+[Table("cheque_distribute", Schema = "cts")]
+public partial class ChequeDistribute
+{
+    [Column("id")]
+    public long? Id { get; set; }
+
+    [Column("range")]
+    public NpgsqlRange<int>[]? Range { get; set; }
+
+    [Column("quantity")]
+    public short? Quantity { get; set; }
+
+    [Column("distribute_to", TypeName = "char")]
+    public char? DistributeTo { get; set; }
+
+    [Column("cheque_series_info", TypeName = "char")]
+    public char? ChequeSeriesInfo { get; set; }
+
+    [Column("user_id")]
+    public short? UserId { get; set; }
+
+    [Column("received_id")]
+    public short? ReceivedId { get; set; }
+
+    [Column("created_at")]
+    public DateTime? CreatedAt { get; set; }
+}
