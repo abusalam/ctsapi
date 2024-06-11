474c9abcdfe7bacf5beca4c01e994f453a059875
itsmanwar <102802707+itsmanwar@users.noreply.github.com>
2024-06-11 12:30:20 +0530

    temp to zip & publish file include

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -23,201 +23,15 @@ namespace CTS_BE.Controllers
             _paymandateService = paymandateService;
             _transactionLotService = transactionLotService;
         }
-        //[HttpGet("GeneretXML")]
-        //public void GenerateXML()
-        //{
-        //    string filePath = "example.xml";
-        //    XmlWriterSettings settings = new XmlWriterSettings
-        //    {
-        //        Indent = true,
-        //        IndentChars = "\t",
-        //        Encoding = System.Text.Encoding.UTF8
-        //    };
-
-        //    using (XmlWriter writer = XmlWriter.Create(filePath, settings))
-        //    {
-        //        writer.WriteStartDocument();
-
-        //        // Start RequestPayload element
-        //        writer.WriteStartElement("RequestPayload");
-
-        //        // Write AppHdr element
-        //        writer.WriteStartElement("AppHdr");
-
-        //        // Write Fr element
-        //        writer.WriteStartElement("Fr");
-        //        writer.WriteStartElement("OrgId");
-        //        writer.WriteStartElement("Id");
-        //        writer.WriteStartElement("OrgId");
-        //        writer.WriteStartElement("Othr");
-        //        writer.WriteElementString("Id", "125");
-        //        writer.WriteEndElement(); // Othr
-        //        writer.WriteEndElement(); // OrgId
-        //        writer.WriteEndElement(); // Id
-        //        writer.WriteEndElement(); // OrgId
-        //        writer.WriteEndElement(); // Fr
-
-        //        // Write To element
-        //        writer.WriteStartElement("To");
-        //        writer.WriteStartElement("FIId");
-        //        writer.WriteStartElement("FinInstnId");
-        //        writer.WriteStartElement("ClrSysMmbId");
-        //        writer.WriteElementString("MmbId", "RBI");
-        //        writer.WriteEndElement(); // ClrSysMmbId
-        //        writer.WriteEndElement(); // FinInstnId
-        //        writer.WriteEndElement(); // FIId
-        //        writer.WriteEndElement(); // To
-
-        //        writer.WriteElementString("BizMsgIdr", "EPV80125000000000210202402100338");
-        //        writer.WriteElementString("MsgDefIdr", "pain.001.001.08");
-        //        writer.WriteElementString("BizSvc", "CustomerCreditTransferInitiationV08");
-        //        writer.WriteElementString("CreDt", "2024-02-10T14:02:46");
-
-        //        writer.WriteEndElement(); // AppHdr
-
-        //        // Write Document element
-        //        writer.WriteStartElement("Document");
-
-        //        // Write CstmrCdtTrfInitn element
-        //        writer.WriteStartElement("CstmrCdtTrfInitn");
-
-        //        // Write GrpHdr element
-        //        writer.WriteStartElement("GrpHdr");
-        //        writer.WriteElementString("MsgId", "EPV80125000000000210202402100338");
-        //        writer.WriteElementString("CreDtTm", "2024-02-10T14:02:46");
-        //        writer.WriteStartElement("Authstn");
-        //        writer.WriteElementString("Prtry", "ALL");
-        //        writer.WriteEndElement(); // Authstn
-        //        writer.WriteElementString("NbOfTxs", "2");
-        //        writer.WriteElementString("CtrlSum", "41701.00");
-
-        //        // Write InitgPty element
-        //        writer.WriteStartElement("InitgPty");
-        //        writer.WriteElementString("Nm", "JHARKHAND");
-        //        writer.WriteStartElement("Id");
-        //        writer.WriteStartElement("OrgId");
-        //        writer.WriteStartElement("Othr");
-        //        writer.WriteElementString("Id", "125");
-        //        writer.WriteEndElement(); // Othr
-        //        writer.WriteEndElement(); // OrgId
-        //        writer.WriteEndElement(); // Id
-        //        writer.WriteStartElement("CtctDtls");
-        //        writer.WriteElementString("EmailAdr", "test@gmail.com");
-        //        writer.WriteEndElement(); // CtctDtls
-        //        writer.WriteEndElement(); // InitgPty
-
-        //        writer.WriteEndElement(); // GrpHdr
-
-        //        // Write PmtInf element
-        //        writer.WriteStartElement("PmtInf");
-        //        writer.WriteElementString("PmtInfId", "0125CKP0002100020240210202402");
-        //        writer.WriteElementString("PmtMtd", "TRF");
-        //        writer.WriteElementString("BtchBookg", "true");
-        //        writer.WriteElementString("NbOfTxs", "2");
-        //        writer.WriteElementString("CtrlSum", "41701.00");
-        //        writer.WriteStartElement("PmtTpInf");
-        //        writer.WriteElementString("InstrPrty", "HIGH");
-        //        writer.WriteStartElement("SvcLvl");
-        //        writer.WriteElementString("Prtry", "NEFT");
-        //        writer.WriteEndElement(); // SvcLvl
-        //        writer.WriteEndElement(); // PmtTpInf
-        //        writer.WriteElementString("ReqdExctnDt", "2024-02-10");
-
-        //        // Write Dbtr element
-        //        writer.WriteStartElement("Dbtr");
-        //        writer.WriteElementString("Nm", "FINANCE DEPT GoJH");
-        //        writer.WriteStartElement("PstlAdr");
-        //        writer.WriteElementString("Dept", "CKP");
-        //        writer.WriteElementString("SubDept", "CKPRWD001");
-        //        writer.WriteEndElement(); // PstlAdr
-        //        writer.WriteStartElement("Id");
-        //        writer.WriteStartElement("OrgId");
-        //        writer.WriteStartElement("Othr");
-        //        writer.WriteElementString("Id", "125");
-        //        writer.WriteEndElement(); // Othr
-        //        writer.WriteEndElement(); // OrgId
-        //        writer.WriteEndElement(); // Id
-        //        writer.WriteEndElement(); // Dbtr
-
-        //        // Write DbtrAcct element
-        //        writer.WriteStartElement("DbtrAcct");
-        //        writer.WriteStartElement("Id");
-        //        writer.WriteStartElement("Othr");
-        //        writer.WriteElementString("Id", "01602501045");
-        //        writer.WriteEndElement(); // Othr
-        //        writer.WriteEndElement(); // Id
-        //        writer.WriteEndElement(); // DbtrAcct
-
-        //        // Write DbtrAgt element
-        //        writer.WriteStartElement("DbtrAgt");
-        //        writer.WriteStartElement("FinInstnId");
-        //        writer.WriteStartElement("ClrSysMmbId");
-        //        writer.WriteElementString("MmbId", "RBIS0GOJHEP");
-        //        writer.WriteEndElement(); // ClrSysMmbId
-        //        writer.WriteEndElement(); // FinInstnId
-        //        writer.WriteEndElement(); // DbtrAgt
-
-        //        // Write CdtTrfTxInf elements
-        //        for (int i = 0; i < 2; i++)
-        //        {
-        //            writer.WriteStartElement("CdtTrfTxInf");
-        //            writer.WriteStartElement("PmtId");
-        //            writer.WriteElementString("InstrId", (i == 0) ? "DRNPAYEE03891A21010022024CKP" : "20RCHR00508E1DNA21010022024CKP");
-        //            writer.WriteElementString("EndToEndId", (i == 0) ? "EP012500000000039824041001576" : "EP012500000000039824041001577");
-        //            writer.WriteEndElement(); // PmtId
-        //            writer.WriteStartElement("Amt");
-        //            writer.WriteStartElement("InstdAmt");
-        //            writer.WriteElementString("Amt", (i == 0) ? "40979.00" : "722.00");
-        //            writer.WriteElementString("CcyOfTrf", "INR");
-        //            writer.WriteEndElement(); // InstdAmt
-        //            writer.WriteEndElement(); // Amt
-        //            writer.WriteStartElement("CdtrAgt");
-        //            writer.WriteStartElement("FinInstnId");
-        //            writer.WriteStartElement("ClrSysMmbId");
-        //            writer.WriteElementString("MmbId", (i == 0) ? "SBIN0001672" : "RBIS0GSTPMT");
-        //            writer.WriteEndElement(); // ClrSysMmbId
-        //            writer.WriteEndElement(); // FinInstnId
-        //            writer.WriteStartElement("BrnchId");
-        //            writer.WriteElementString("Id", (i == 0) ? "SBIN0001672" : "RBIS0GSTPMT");
-        //            writer.WriteEndElement(); // BrnchId
-        //            // writer.WriteEndElement(); // FinInstnId
-        //            writer.WriteEndElement(); // CdtrAgt
-        //            writer.WriteStartElement("Cdtr");
-        //            writer.WriteElementString("Nm", (i == 0) ? "RAIDER SECURITY SERVICES PVT LTD" : "GST");
-        //            writer.WriteEndElement(); // Cdtr
-        //            writer.WriteStartElement("CdtrAcct");
-        //            writer.WriteStartElement("Id");
-        //            writer.WriteStartElement("Othr");
-        //            writer.WriteElementString("Id", (i == 0) ? "33544440518" : "24022000007067");
-        //            writer.WriteEndElement(); // Othr
-        //            writer.WriteEndElement(); // Id
-        //            writer.WriteStartElement("Tp");
-        //            writer.WriteElementString("Cd", (i == 0) ? "10" : "10");
-        //            writer.WriteEndElement(); // Tp
-        //            writer.WriteEndElement(); // CdtrAcct
-        //            writer.WriteEndElement(); // CdtTrfTxInf
-        //        }
-
-        //        writer.WriteEndElement(); // PmtInf
-
-        //        writer.WriteEndElement(); // CstmrCdtTrfInitn
-
-        //        writer.WriteEndElement(); // Document
-
-        //        writer.WriteEndElement(); // RequestPayload
-
-        //        writer.WriteEndDocument();
-        //    }
-        //}
         [HttpGet("PushtoRBI/{fileName}")]
         public async Task<APIResponse<bool>> PushtoRBI(string fileName)
         {
             APIResponse<bool> aPIResponse = new();
             try
             {
-
-                SFTPHelper.UploadFile("1.6.198.15", 22, "GOWB", "Gowb1234$", fileName + ".zip", "/EPAY/IN/" + fileName + ".zip");
-
+                string filePath = "./" + fileName + ".zip";
+                SFTPHelper.UploadFile("1.6.198.15", 22, "GOWB", "Gowb1234$", filePath, "EPAY/IN//" + fileName + ".temp");
+                SFTPHelper.RenameFile("1.6.198.15", 22, "GOWB", "Gowb1234$", fileName + ".temp", fileName + ".zip", "EPAY/IN//");
                 aPIResponse.Message = "Pushed to RBI Successfully";
                 aPIResponse.result = true;
                 aPIResponse.apiResponseStatus = Enum.APIResponseStatus.Success;
@@ -233,7 +47,7 @@ namespace CTS_BE.Controllers
         }
 
         [HttpGet("GeneretXML")]
-        public async Task<APIResponse<string>> GenerateXMLAsync()
+        public async Task<APIResponse<string>> GenerateXMLAsync([FromQuery] bool pushFile = true)
         {
             APIResponse<string> aPIResponse = new();
             string fileName = "";
@@ -256,14 +70,16 @@ namespace CTS_BE.Controllers
                             zip.CreateEntryFromFile(fileName + ".xml", fileName + ".xml");
                             zip.CreateEntryFromFile(fileName + ".sig", fileName + ".sig");
                         }
-                        filePath = "./" + fileName + ".zip";
                         //var tstFile = File.OpenRead(filePath);
-                        SFTPHelper.UploadFile("1.6.198.15", 22, "GOWB", "Gowb1234$", filePath, "EPAY/IN//" + fileName + ".zip");
+                        if (pushFile)
+                        {
+                            filePath = "./" + fileName + ".zip";
+                            SFTPHelper.UploadFile("1.6.198.15", 22, "GOWB", "Gowb1234$", filePath, "EPAY/IN//" + fileName + ".temp");
+                            SFTPHelper.RenameFile("1.6.198.15", 22, "GOWB", "Gowb1234$", fileName + ".temp", fileName + ".zip", "EPAY/IN//");
+                        }
                     }
                     break;
                 }
-
-
                 aPIResponse.Message = "XML Generated & Signed Successfully";
                 aPIResponse.result = fileName;
                 aPIResponse.apiResponseStatus = Enum.APIResponseStatus.Success;
@@ -273,29 +89,6 @@ namespace CTS_BE.Controllers
             {
                 aPIResponse.Message = ex.Message;
                 aPIResponse.result = filePath;
-                aPIResponse.apiResponseStatus = Enum.APIResponseStatus.Error;
-                return aPIResponse;
-            }
-        }
-        [HttpGet("VerifyXML")]
-        public APIResponse<bool> VerifyXML()
-        {
-            APIResponse<bool> aPIResponse = new();
-            try
-            {
-                System.IO.FileInfo xmlfile = new System.IO.FileInfo("./ACK/EPV80116001516701174202404150001" + ".xml");
-                System.IO.FileStream sigfile = new System.IO.FileStream("./ACK/temp.sig", System.IO.FileMode.Open);
-                bool verify = SignHelper.verifySignaturesRBI(xmlfile, sigfile);
-                sigfile.Close();
-                aPIResponse.result = verify;
-                aPIResponse.Message = (verify) ? "Valid XML" : "Invalid XML";
-                aPIResponse.apiResponseStatus = Enum.APIResponseStatus.Success;
-                return aPIResponse;
-            }
-            catch (Exception ex)
-            {
-                aPIResponse.Message = ex.Message;
-                aPIResponse.result = false;
                 aPIResponse.apiResponseStatus = Enum.APIResponseStatus.Error;
                 return aPIResponse;
             }
@@ -314,7 +107,7 @@ namespace CTS_BE.Controllers
                 string xmlSigFilePath = "./ACK/" + xmlSigFileName;
                 // SFTPHelper.MoveFile("10.176.100.62", 22, "admin", "admin", "/CTS/", "/CTS/Done/", fileName + ".zip");
                 // SFTPHelper.DownloadFile("1.6.198.15", 22, "GOWB", "Gowb1234$", "/EPAY/INACK" + fileName + ".zip", localFilePath);
-                SFTPHelper.DownloadFile("10.176.100.62", 22, "admin", "admin", "/CTS/" + fileName + ".zip", localFilePath);
+                SFTPHelper.DownloadFile("1.6.198.15", 22, "GOWB", "Gowb1234$", "/CTS/" + fileName + ".zip", localFilePath);
                 using (ZipArchive archive = ZipFile.OpenRead(localFilePath))
                 {
                     foreach (ZipArchiveEntry entry in archive.Entries)
@@ -325,7 +118,7 @@ namespace CTS_BE.Controllers
                 if (SignHelper.VerifyXMLSignatures(xmlFilePath, xmlSigFilePath))
                 {
                     // SFTPHelper.MoveFile("10.176.100.62", 22, "admin", "admin","/CTS/"+xmlFileName,"/CTS/Done/"+xmlFileName,fileName+".zip");
-                    SFTPHelper.MoveFile("10.176.100.62", 22, "admin", "admin", "/CTS/", "/CTS/Done/", fileName + ".zip");
+                    SFTPHelper.MoveFile("1.6.198.15", 22, "GOWB", "Gowb1234$", "/CTS/", "/CTS/Done/", fileName + ".zip");
                     aPIResponse.Message = "INACK Downloaded and Verified Successfully";
                     aPIResponse.result = "true";
                     aPIResponse.apiResponseStatus = Enum.APIResponseStatus.Success;
--- Helper/SFTPHelper.cs
+++ Helper/SFTPHelper.cs
@@ -10,13 +10,13 @@ namespace CTS_BE.Helper
 {
     public static class SFTPHelper
     {
-        public static void UploadFile(string hostServerName, short port, string userName, string password,string localPath,string uploadPath)
+        public static void UploadFile(string hostServerName, short port, string userName, string password, string localPath, string uploadPath)
         {
             using SftpClient client = new(hostServerName, port, userName, password);
 
             try
             {
-                if(!File.Exists(localPath))
+                if (!File.Exists(localPath))
                 {
                     throw new Exception("Local file not found!");
                 }
@@ -30,25 +30,25 @@ namespace CTS_BE.Helper
             }
             catch (Exception e) when (e is SshConnectionException || e is SocketException || e is ProxyException)
             {
-               throw new Exception($"Error connecting to server: {e.Message}");
+                throw new Exception($"Error connecting to server: {e.Message}");
             }
             catch (SshAuthenticationException e)
             {
-                 throw new Exception($"Failed to authenticate: {e.Message}");
+                throw new Exception($"Failed to authenticate: {e.Message}");
             }
             catch (SftpPermissionDeniedException e)
             {
-                 throw new Exception($"Operation denied by the server: {e.Message}");
+                throw new Exception($"Operation denied by the server: {e.Message}");
             }
             catch (SshException e)
             {
-                 throw new Exception($"Sftp Error: {e.Message}");
+                throw new Exception($"Sftp Error: {e.Message}");
             }
         }
         public static void DownloadFile(string hostServerName, short port, string userName, string password, string remotePath, string localPath)
         {
             using SftpClient client = new(hostServerName, port, userName, password);
-        
+
             try
             {
                 client.Connect();
@@ -88,7 +88,7 @@ namespace CTS_BE.Helper
         public static void MoveFile(string hostServerName, short port, string userName, string password, string sourcePath, string destinationPath, string fileName)
         {
             using SftpClient client = new(hostServerName, port, userName, password);
-        
+
             try
             {
                 client.Connect();
@@ -96,7 +96,7 @@ namespace CTS_BE.Helper
                 {
                     string sourceFilePath = Path.Combine(sourcePath, fileName);
                     string destinationFilePath = Path.Combine(destinationPath, fileName);
-        
+
                     if (client.Exists(sourceFilePath))
                     {
                         client.RenameFile(sourceFilePath, destinationFilePath);
@@ -105,7 +105,45 @@ namespace CTS_BE.Helper
                     {
                         throw new Exception($"File '{fileName}' does not exist in the source folder.");
                     }
-        
+
+                    client.Disconnect();
+                }
+            }
+            catch (Exception e) when (e is SshConnectionException || e is SocketException || e is ProxyException)
+            {
+                throw new Exception($"Error connecting to server: {e.Message}");
+            }
+            catch (SshAuthenticationException e)
+            {
+                throw new Exception($"Failed to authenticate: {e.Message}");
+            }
+            catch (SftpPermissionDeniedException e)
+            {
+                throw new Exception($"Operation denied by the server: {e.Message}");
+            }
+            catch (SshException e)
+            {
+                throw new Exception($"Sftp Error: {e.Message}");
+            }
+        }
+        public static void RenameFile(string hostServerName, short port, string userName, string password, string oldFileName, string newFileName,string remotePath)
+        {
+            using SftpClient client = new(hostServerName, port, userName, password);
+
+            try
+            {
+                client.Connect();
+                if (client.IsConnected)
+                {
+                    if (client.Exists(Path.Combine(remotePath, oldFileName)))
+                    {
+                        client.RenameFile(Path.Combine(remotePath, oldFileName), Path.Combine(remotePath, newFileName));
+                    }
+                    else
+                    {
+                        throw new Exception($"File '{oldFileName}' does not exist on the remote server.");
+                    }
+
                     client.Disconnect();
                 }
             }
