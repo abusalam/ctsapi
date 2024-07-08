1a187a058d822e963dd2cb7b216296e19b0f0bdb
Soumyaneel Sarkar <65025579+A-Little-Hat@users.noreply.github.com>
2024-07-08 17:34:47 +0530

    Stamp Requisition Enum created

---
+++ Enum/StampRequisitionEnum.cs
@@ -0,0 +1,19 @@
+﻿namespace CTS_BE.Enum
+{
+    public enum StampRequisitionStatusEnum
+    {
+        ForwardedToStampCleck = 30,
+
+        ForwardedToTreasuryOfficer = 31,
+        RejectByStampClerk = 32,
+
+        WaitingForPayment = 33,
+        RejectByTreasuryOfficer = 34,
+
+        WaitingForTreasuryOfficerVerification = 35,
+        VerifiedByTreasuryOfficer = 36,
+
+        DeliveredToVendor = 37
+    }
+
+}
