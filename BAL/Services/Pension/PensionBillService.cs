f6bffcfeef551841231c78464cf8a68173fb7e03
Abu Salam [Lenovo-Slim3i] <abu.alam@nic.in>
2024-09-07 16:13:10 +0530

    Restrict Generation of First Pension Bill without Bank Account

--- BAL/Services/Pension/PensionBillService.cs
+++ BAL/Services/Pension/PensionBillService.cs
@@ -58,12 +58,25 @@ namespace CTS_BE.BAL.Services.Pension
                 );
 
             if(pensioner == null) {
-                T errResponse = default!;
+                InitiateFirstPensionBillResponseDTO errResponse = new ();
                 errResponse.FillDataSource(
                     pensioner,
                     "No such PPO exists"
                 );
-                return errResponse;
+                return _mapper.Map<T>(errResponse);
+            }
+
+            pensionDbContext.Entry(pensioner)
+                .Collection(entity => entity.BankAccounts)
+                .Query().Where(entity => entity.ActiveFlag)
+                .Load();
+            if(pensioner.BankAccounts.Count == 0) {
+                InitiateFirstPensionBillResponseDTO errResponse = new ();
+                errResponse.FillDataSource(
+                    pensioner,
+                    "No bank account exists"
+                );
+                return _mapper.Map<T>(errResponse);
             }
 
             pensionDbContext.Entry(pensioner)
@@ -83,85 +96,11 @@ namespace CTS_BE.BAL.Services.Pension
                 .Query().Where(entity => entity.ActiveFlag)
                 .Load();
 
-            pensionDbContext.Entry(pensioner)
-                .Collection(entity => entity.BankAccounts)
-                .Query().Where(entity => entity.ActiveFlag)
-                .Load();
-
-            // List<PpoPaymentListItemDTO>? ppoPayments = new();
-
-            // DateOnly calculatedPeriodStartDate = initiateFirstPensionBillDTO.ToDate;
-            // long prevBreakupId = 0;
             foreach (ComponentRate componentRate in pensioner.Category.ComponentRates) {
                 pensionDbContext.Entry(componentRate)
                 .Reference(entity => entity.Breakup)
                 .Load();
-                // $"{componentRate.Breakup.Id}, {componentRate.Breakup.ComponentName}".PrintOut();
-                // if(componentRate.Breakup.Id != prevBreakupId) {
-                //     calculatedPeriodStartDate = initiateFirstPensionBillDTO.ToDate;
-                // }
-                // prevBreakupId = componentRate.Breakup.Id;
-                // ppoPayments.Add(new PpoPaymentListItemDTO()
-                // {
-                //     RateId = componentRate.Id,
-                //     ComponentName = componentRate.Breakup.ComponentName,
-                //     RateType = componentRate.RateType,
-                //     RateAmount = componentRate.RateAmount,
-                //     BasicPensionAmount = pensioner.BasicPensionAmount,
-                //     BreakupAmount = PensionCalculator.CalculatePerMonthBreakupAmount(
-                //         PensionCalculator.CalculateEffectiveRate(
-                //             pensioner.Category.ComponentRates.ToList(),
-                //             componentRate.Breakup.Id,
-                //             calculatedPeriodStartDate      
-                //         ),
-                //         pensioner.BasicPensionAmount
-                //         ),
-                //     FromDate = PensionCalculator.CalculatePeriodStartFromDate(
-                //             PensionCalculator.CalculateEffectiveRate(
-                //                     pensioner.Category.ComponentRates.ToList(),
-                //                     componentRate.Breakup.Id,
-                //                     calculatedPeriodStartDate.AddDays(-1)
-                //                 ).EffectiveFromDate,
-                //             pensioner.DateOfCommencement
-                //         ),
-                //     ToDate = calculatedPeriodStartDate.AddDays(-1),
-                // });
-                // calculatedPeriodStartDate = componentRate.EffectiveFromDate;
             }
-
-            // PensionCalculator.CalculatePpoPaymentStartFromDate(ref ppoPayments, pensioner.DateOfCommencement);
-
-            // List<ComponentRate> ppoComponentRate = new();
-            // List<long> componentBreakupIds = new();
-            // componentBreakupIds = PensionCalculator.CalculateBreakups(pensioner.Category.ComponentRates.ToList());
-            // componentBreakupIds.ForEach(breakupId => {
-            //     ppoComponentRate.Add(PensionCalculator.CalculateEffectiveRate(
-            //             pensioner.Category.ComponentRates.ToList(),
-            //             breakupId,
-            //             initiateFirstPensionBillDTO.ToDate      
-            //         ));
-            // });
-
-            // dynamic dataSource = new ExpandoObject();
-            // dataSource.DateValue = initiateFirstPensionBillDTO.ToDate;
-            // dataSource.BreakupIds = componentBreakupIds;
-            // dataSource.RateIds = _mapper.Map<List<ComponentRateResponseDTO>>(ppoComponentRate);
-            // dataSource.MinDateValue = PensionCalculator.CalculatePeriodStartDate(initiateFirstPensionBillDTO.ToDate);
-            // dataSource.MaxDateValue = PensionCalculator.CalculatePeriodEndDate(initiateFirstPensionBillDTO.ToDate);
-
-            // dataSource.Pensioner = _mapper.Map<PensionerResponseDTO>(pensioner);
-            // dataSource.PpoPayments = PensionCalculator.CalculatePpoPayments(
-            //         pensioner.Category.ComponentRates,
-            //         pensioner.DateOfCommencement,
-            //         initiateFirstPensionBillDTO.ToDate,
-            //         pensioner.BasicPensionAmount
-            //     );
-            // dataSource.PpoTotalPaymentPerMonth = ppoPayments.Sum(entity => entity.BreakupAmount);
-            // dataSource.PpoFirstBillTotalAmount = PensionCalculator.CalculateTotalPensionAmount(
-            //     pensioner.Category.ComponentRates.ToList(),
-            //     pensioner.BasicPensionAmount,
-            //     12
-            // );
             List<PpoPaymentListItemDTO>? ppoPayments = PensionCalculator.CalculatePpoPayments(
                         pensioner.Category.ComponentRates,
                         pensioner.DateOfCommencement,
@@ -187,15 +126,6 @@ namespace CTS_BE.BAL.Services.Pension
                 });
             }
             InitiateFirstPensionBillResponseDTO response = new (){
-                // DataSource = dataSource,
-                // Pensioner = _mapper.Map<PensionerListItemDTO>(pensioner),
-                // BankAccount = _mapper.Map<PensionerBankAcResponseDTO>(
-                //         await _pensionerBankAccountRepository.GetSingleAysnc(
-                //             entity => entity.ActiveFlag
-                //             && entity.PpoId==initiateFirstPensionBillDTO.PpoId
-                //             && entity.TreasuryCode==treasuryCode
-                //         )
-                //     ),
                 PpoId = initiateFirstPensionBillDTO.PpoId,
                 // PensionCategory = _mapper.Map<PensionCategoryResponseDTO>(pensioner.Category),
                 // ComponentRates = _mapper.Map<List<ComponentRateResponseDTO>>(pensioner.Category.ComponentRates)
@@ -219,23 +149,6 @@ namespace CTS_BE.BAL.Services.Pension
                 response.GrossAmount = response.PpoBillBreakups.ToList().Sum(entity => entity.DueAmount);
                 response.NetAmount = response.PpoBillBreakups.ToList().Sum(entity => entity.NetAmount);
             }
-
-
-            // ************************************
-            // Save Pensioner Component Revisions
-            // ************************************
-
-            // pensioner.PpoComponentRevisions = _mapper.Map<List<PpoComponentRevision>>(response.PensionerPayments.ToList());
-
-            // pensioner.PpoComponentRevisions.ToList().ForEach(entity => {
-            //     entity.TreasuryCode = treasuryCode;
-            //     entity.ActiveFlag = true;
-            //     entity.PpoId = initiateFirstPensionBillDTO.PpoId;
-            //     entity.ToDate = (initiateFirstPensionBillDTO.ToDate == entity.ToDate) ? null : entity.ToDate;
-            // });
-
-            // pensionDbContext.Pensioners.Update(pensioner);
-            // await pensionDbContext.SaveChangesAsync();
             return _mapper.Map<T>(response);
         }
     }
