f049b5e09815683fc17708b91f94e6d06b7e609f
Nilakshi Chakraborty <chakraborty.nilakshi2004@gmail.com>
2025-03-17 07:22:46 +0530

    Enable all playwright tests

--- Seeders/Pension/BreakupSeeder.cs
+++ Seeders/Pension/BreakupSeeder.cs
@@ -17,7 +17,7 @@ namespace CTS_BE.Seeders.Pension
             // Create the "Basic Pension" component first with id=1
             var basicPensionBreakup = new Breakup
             {
-                ComponentName = "Basic Pension",
+                ComponentName = "BASIC PENSION",
                 ComponentType = 'P',
                 ReliefFlag = false,
                 CreatedBy = 1,
@@ -25,9 +25,19 @@ namespace CTS_BE.Seeders.Pension
             };
 
             context.Breakups.Add(basicPensionBreakup);
+
+            var amountCommutedBreakup = new Breakup
+            {
+                ComponentName = "AMOUNT COMMUTED",
+                ComponentType = 'D',
+                ReliefFlag = false,
+                CreatedBy = 39,
+                ActiveFlag = true,
+            };
+            context.Breakups.Add(amountCommutedBreakup);
             context.SaveChanges();
 
-            int additionalCount = Math.Max(count - 1, 7);
+            int additionalCount = 6;
 
             var breakups = new ComponentFactory()
                 .Make(additionalCount)
